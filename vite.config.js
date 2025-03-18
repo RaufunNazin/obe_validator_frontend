9f02e450ace74027d109b68ea33bce305a1c01d5
RaufunNazin <raufun.nazin13@gmail.com>
2025-03-18 16:19:48 +0600

    Fix: Set Vite to use Railway's PORT

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // Ensures it listens on all interfaces
+    port: process.env.PORT || 5173, // Use Railway's assigned port
+  }
 });
